c1235e654f4a4fada4fbf561f8db7044fc829989
JB <jmb2883@gmail.com>
2018-04-21 08:56:11 -0400

    changing to search api instead of random

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 //declaring the plants array with intial values
-  var plants = ["anenome", "tree", "tulips", "geranium", "crocus"];
+  var plants = ["tree", "tulips", "geranium", "crocus"];
 
   //creating a function to add buttons based on the number of plants in the array
   function addButton() {
@@ -9,17 +9,48 @@ $(document).ready(function () {
       var plantButton = $("<button>");
       plantButton.append(plants[i]);
       plantButton.attr("data-value", plants[i]);
-      $("#imageButton").prepend(plantButton);
+      $("#imageButton").append(plantButton);
     };
   };
 //on click function that generates the request/response from giphy and displays the images on the screen
-  $("#imageButton").on("click", "button", function () {
+  // $("#imageButton").on("click", "button", function () {
 
-    var imageInput = $(this).attr("data-value")
+  //   var imageInput = $(this).attr("data-value")
+  //   console.log(imageInput)
+  //   // setting a variable for the api
+
+  //   var queryURL = "https://api.giphy.com/v1/gifs/random?tag=" + imageInput + "&api_key=dc6zaTOxFJmzC";
+  //   console.log(queryURL)
+
+  //   //creating the request to get data from the api
+  //   $.ajax({
+  //       url: queryURL,
+  //       method: "GET"
+  //     })
+
+  //     //the part that handles the response
+  //     .then(function (response) {
+  //       console.log(response)
+
+  //       var imageUrl = response.data.image_original_url;
+  //       var stillImageUrl = response.data.fixed_height_small_still_url;
+  //       var stillImageUrl = response.data.fixed_height_small_still_url;
+  //       var springImage = $("<img>");
+  //       springImage.attr("src", stillImageUrl);
+  //       springImage.attr("data-still", stillImageUrl);
+  //       springImage.attr("data-animate", imageUrl);
+  //       springImage.attr("data-status", "still");
+  //       springImage.attr("alt",  "giphy image");
+  //       $("#images").prepend(springImage);
+  //     });
+  // });
+
+  $("#imageButton").on("click", "button", function () {
+  var imageInput = $(this).attr("data-value")
     console.log(imageInput)
     // setting a variable for the api
 
-    var queryURL = "https://api.giphy.com/v1/gifs/random?tag=" + imageInput + "&api_key=dc6zaTOxFJmzC";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + imageInput + "&api_key=dc6zaTOxFJmzC" + "&limit=10";
     console.log(queryURL)
 
     //creating the request to get data from the api
@@ -31,19 +62,21 @@ $(document).ready(function () {
       //the part that handles the response
       .then(function (response) {
         console.log(response)
-
-        var imageUrl = response.data.image_original_url;
-        var stillImageUrl = response.data.fixed_height_small_still_url;
+     for (i=0; i < response.data.length; i++) {
+        var imageUrl = response.data[i].images.original.url;
+        var stillImageUrl = response.data[i].images.original.url;
+        var rating = response.data[i].rating;
         var springImage = $("<img>");
         springImage.attr("src", stillImageUrl);
         springImage.attr("data-still", stillImageUrl);
         springImage.attr("data-animate", imageUrl);
         springImage.attr("data-status", "still");
         springImage.attr("alt",  "giphy image");
+        // $("#images").append(rating);
         $("#images").prepend(springImage);
+     };
       });
   });
-
 
 
   $("body").on("click", "#search", function () {
