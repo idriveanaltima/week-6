2262ed674e67701fbaf49e7e760dddf5f1676a90
JB <jmb2883@gmail.com>
2018-04-23 13:14:18 -0400

    beautify

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   //declaring the plants array with intial values
-  var plants = ["rose", "tulips", "geranium", "crocus", "pansy",];
+  var plants = ["rose", "tulips", "geranium", "crocus", "pansy", ];
 
   //creating a function to add buttons based on the number of plants in the array
   function addButton() {
@@ -23,54 +23,52 @@ $(document).ready(function () {
         method: "GET",
       })
       .then(function (response) {
-       
+
         for (i = 0; i < response.data.length; i++) {
           var imageUrl = response.data[i].images.fixed_height_small.url;
           var stillImageUrl = response.data[i].images.fixed_height_small_still.url;
           var rating = response.data[i].rating.toUpperCase();
           var title = response.data[i].title;
-          if(rating == "PG" || rating == "G") {       
-          var springImage = $("<img>");
-          springImage.addClass("img img-thumbnail");
-          springImage.attr({
-            src: stillImageUrl,
-            alt: title,
-            "data-still": stillImageUrl,
-            "data-animate": imageUrl,
-            "data-status": "still",
-            "width": "100px",
-          });
-          var newDiv = $("<div class='imgBox p-3 m-3'>");
-          var titleSpan = $("<span>");
-          var ratingSpan = $("<span>");
-          ratingSpan.css({
-                "display": "block",  
-          });
-          newDiv.css({
-                "width": "auto",
-                "height": "auto",
-                "float": "left",
-                "border": "2px solid black"
-          });
-          newDiv.prependTo("#images")
-          ratingSpan.prependTo(newDiv);
-          titleSpan.prependTo(newDiv);
-          ratingSpan.prepend("Rating: " + rating);
-          titleSpan.prepend(title);
-          newDiv.append(springImage);          
-        }{
+          if (rating == "PG" || rating == "G") {
+            var springImage = $("<img>");
+            springImage.addClass("img img-thumbnail");
+            springImage.attr({
+              src: stillImageUrl,
+              alt: title,
+              "data-still": stillImageUrl,
+              "data-animate": imageUrl,
+              "data-status": "still",
+              "width": "100px",
+            });
+            var newDiv = $("<div class='imgBox p-3 m-3'>");
+            var titleSpan = $("<span>");
+            var ratingSpan = $("<span>");
+            ratingSpan.css({
+              "display": "block",
+            });
+            newDiv.css({
+              "width": "auto",
+              "height": "auto",
+              "float": "left",
+              "border": "2px solid black"
+            });
+            newDiv.prependTo("#images")
+            ratingSpan.prependTo(newDiv);
+            titleSpan.prependTo(newDiv);
+            ratingSpan.prepend("Rating: " + rating);
+            titleSpan.prepend(title);
+            newDiv.append(springImage);
+          } {};
         };
-      };
       });
   });
   $("body").on("click", "#add", function () {
     var input = $("#input").val().trim();
     if (plants.indexOf(input) == -1) {
-    plants.push(input);
-    addButton();
-    }{
-    };
-   }).on("click", "img", function () {
+      plants.push(input);
+      addButton();
+    } {};
+  }).on("click", "img", function () {
     var status = $(this).attr("data-status");
     if (status === "still") {
       $(this).attr("src", $(this).attr("data-animate"));
